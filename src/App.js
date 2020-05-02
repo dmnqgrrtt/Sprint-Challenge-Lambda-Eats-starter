d69a36b0c2484e4c55b9209872433bd64dc376d4
Dominique Garrett <dmnqgrrtt@gmail.com>
2020-05-01 23:09:52 -0400

    created components and created routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from "react";
+import { Route, Switch } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import OrderPage from './components/OrderPage';
 
 const App = () => {
   return (
     <>
       <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+      <Switch>
+        <Route path='/pizza'>
+          <OrderPage />
+        </Route>
+        <Route path='/'>
+          <HomePage />
+        </Route>
+      </Switch>
     </>
   );
 };
